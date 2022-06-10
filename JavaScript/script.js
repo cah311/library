ae4419576231caac3902dad913c95f1d55995ba9
Carson <c_halter@live.ca>
2022-06-10 14:02:29 -0600

    begining constructor function for library

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -10,10 +10,19 @@ function Book(name, author, pages, read) {
     }
 }
 
-function addBookToLibary() {
-    // do stuff here
-}
+
 
 const book1 = new Book('The Hobbit', "J.R.R. Tolkien", "295 pages", "have read")
-console.log(book1.info())
+const book2 = new Book('LOTR 1', "J.R.R. Tolkien", "2195 pages", "have read")
+const book3 = new Book('LORT 2', "J.R.R. Tolkien", "2323 pages", "have read")
+
+function addBookToLibary(x) {
+    myLibrary.unshift(x)
+};
+
+addBookToLibary(book1)
+addBookToLibary(book2)
+addBookToLibary(book3)
+
+console.log(myLibrary)
 
