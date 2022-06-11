0310d475b8fd7f545e40b24daacdd18c540c88bb
Carson <c_halter@live.ca>
2022-06-11 07:30:35 -0600

    Set initial input html

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -10,8 +10,6 @@ function Book(name, author, pages, read) {
     }
 }
 
-
-
 const book1 = new Book('The Hobbit', "J.R.R. Tolkien", "295 pages", "have read")
 const book2 = new Book('LOTR 1', "J.R.R. Tolkien", "2195 pages", "have read")
 const book3 = new Book('LORT 2', "J.R.R. Tolkien", "2323 pages", "have read")
