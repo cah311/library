c94abed0e474dc44f7a2cb0fa7280ae992152f2a
Carson <c_halter@live.ca>
2022-06-12 16:00:39 -0600

    place new book at start

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+
 const librarySection = document.querySelector('#libraryContainer')
 
 function Book() {
@@ -6,8 +7,8 @@ function Book() {
     this.author = document.getElementById('authorInput').value
     this.pages = document.getElementById('pagesInput').value
     this.read = readToggle()
-    readToggle()
-    return (this.name, this.author, this.pages, readToggle())
+
+    return (this.name, this.author, this.pages, this.read)
     
 }
 
@@ -17,28 +18,38 @@ function readToggle() {
     if (readToggle.checked) {
         status = 'read'
         return status
-        console.log(status)
     } else {
         status = 'not read'
         return status
-        console.log(status)
+        
     }
     
 }
 
 function addBookToLibrary() {
-    let newBookArray = new Book()
-    let newBook = Object.values(newBookArray)
-    myLibrary.unshift(newBook)
-
-
-
-    const library = document.createElement('div');
-    library.classList.add('books')
-    library.textContent = myLibrary[0]
-
-    librarySection.appendChild(library)
     
+    let x = myLibrary.length
+
+    let newBookArray = new Book()   //pulls from Book constructor
+    let newBook = Object.values(newBookArray)   //makes object into text values  
+    myLibrary.unshift(newBook)    //adds new boopk from constructor to from of library array
+
+    let libraryStart = document.getElementById('libraryStart')
+
+    
+    let bookStackTop = document.createElement('div');
+    bookStackTop.setAttribute('id', `book${x}`)
+    bookStackTop.textContent = myLibrary[0]
+
+
+    librarySection.insertBefore(bookStackTop, libraryStart)
+    librarySection.insertBefore( libraryStart, bookStackTop)
+    
+    // librarySection.appendChild(bookStackTop)
+    
+    
+
+    console.log(bookStackTop.id)
     console.log(newBook)
     console.log(myLibrary)
 };
