acd3031fd98dff47a9d1c5c053416413a0ddb1e1
Carson <c_halter@live.ca>
2022-06-12 07:05:29 -0600

    set read toggle value

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,10 +1,4 @@
 let myLibrary = [];
-// let name = document.querySelector('titleInput')
-// let author = document.querySelector('authorInput')
-// let pages = document.querySelector('pagesInput')
-// let read = document.querySelector('pagesInput')
-
-
 
 function Book() {
     this.name = document.getElementById('titleInput').value
@@ -14,27 +8,33 @@ function Book() {
     // this.info = function() {
     //    return name + " by " + author + " , " + pages + " , " + read 
     // }
-    console.log(this.name, this.author, this.pages)
+    readToggle()
+
+    console.log(this.name, this.author, this.pages, readToggle())
     
 }
 
-// function post() {
-//     let a = document.getElementById('titleInput').value;
-//     let b = document.querySelector('#authorInput')
-//     let c = document.querySelector('#pagesInput')
-//     let d = document.querySelector('#pagesInput')
-//     console.log(a)
-// }
-
+function readToggle() {
+    let readToggle = document.querySelector('#readInput')
+    let status;
+    if (readToggle.checked) {
+        status = 'read'
+        return status
+    } else {
+        status = 'not read'
+        return status
+    }
+    
+}
 
 
 // const book1 = new Book('The Hobbit', "J.R.R. Tolkien", "295 pages", "have read")
 // const book2 = new Book('LOTR 1', "J.R.R. Tolkien", "2195 pages", "have read")
 // const book3 = new Book('LORT 2', "J.R.R. Tolkien", "2323 pages", "have read")
 
-// function addBookToLibary(x) {
-//     myLibrary.unshift(x)
-// };
+function addBookToLibary(x) {
+    myLibrary.unshift(x)
+};
 
 // addBookToLibary(book1)
 // addBookToLibary(book2)
