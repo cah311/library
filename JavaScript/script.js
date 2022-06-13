96a4f38207875a03003d84cbc11d0c9e6777963b
Carson <c_halter@live.ca>
2022-06-13 04:54:39 -0600

    add comments

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,6 +2,7 @@ let myLibrary = [];
 
 const librarySection = document.querySelector('#libraryContainer')
 
+//book constructor
 function Book() {
     this.name = document.getElementById('titleInput').value
     this.author = document.getElementById('authorInput').value
@@ -12,6 +13,7 @@ function Book() {
     
 }
 
+//function to give status of read toggle
 function readToggle() {
     let readToggle = document.querySelector('#readInput')
     let status;
@@ -26,6 +28,7 @@ function readToggle() {
     
 }
 
+
 function addBookToLibrary() {
     
     let x = myLibrary.length
@@ -34,40 +37,39 @@ function addBookToLibrary() {
     let newBook = Object.values(newBookArray)   //makes object into text values  
     myLibrary.unshift(newBook)    //adds new boopk from constructor to from of library array
 
-    let libraryStart = document.getElementById('libraryStart')
-
+    let libraryStart = document.getElementById('libraryStart') //gives new book a target to move ontop of
     
+    //container for new book addition
     let bookStackTop = document.createElement('div');
     bookStackTop.classList.add('bookBinding')
     bookStackTop.setAttribute('id', `book${x}`)
 
+    //book title div 
     let bookTitle = document.createElement('div')
     bookTitle.classList.add('bookTitle')
     bookTitle.textContent = (`${newBookArray.name}`)
 
+    //book author div
     let bookAuthor = document.createElement('div')
     bookAuthor.classList.add('bookAuthor')
     bookAuthor.textContent = (`By ${newBookArray.author}`)
     
+    //book pages div
     let bookPages = document.createElement('div')
     bookPages.classList.add('bookPages')
     bookPages.textContent = (` ${newBookArray.pages}`)
 
+    //book read status 
     let bookRead = document.createElement('div')
     bookRead.classList.add('bookRead')
     bookRead.textContent = (` ${newBookArray.read}`)
 
-
-    // bookStackTop.textContent = myLibrary[0]
-    // bookStackTop.textContent = (`${newBookArray.name}`)
-
-
-    librarySection.insertBefore(bookStackTop, libraryStart)
-    bookStackTop.appendChild(bookTitle)
-    bookStackTop.appendChild(bookAuthor)
-    bookStackTop.appendChild(bookPages)
-    bookStackTop.appendChild(bookRead)
-    librarySection.insertBefore( libraryStart, bookStackTop)
+    librarySection.insertBefore(bookStackTop, libraryStart) //inserts new book at the top of the order 
+    bookStackTop.appendChild(bookTitle) //adds title 
+    bookStackTop.appendChild(bookAuthor) //adds author 
+    bookStackTop.appendChild(bookPages) //adds pages 
+    bookStackTop.appendChild(bookRead) //adds read status 
+    librarySection.insertBefore( libraryStart, bookStackTop) //inserts black div back at the top for new book addition to be able to target
     
 
     console.log(bookStackTop.id)
